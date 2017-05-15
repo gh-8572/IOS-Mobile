1cb6c7028df0b78b004b6186827865211573d9ec
es23 <es23@iupui.edu>
2017-05-15 12:55:57 -0400

    Delete

--- HomeInventory/PropertyTableViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PropertyTableViewController.swift
-//  HomeInventory
-//
-//  Created by smith, eric jay on 10/10/16.
-//  Copyright © 2016 smith, eric jay. All rights reserved.
-//
-
-import UIKit
--- HomeInventory/RoomTableViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RoomTableViewController.swift
-//  HomeInventory
-//
-//  Created by smith, eric jay on 10/10/16.
-//  Copyright © 2016 smith, eric jay. All rights reserved.
-//
-
-import UIKit
